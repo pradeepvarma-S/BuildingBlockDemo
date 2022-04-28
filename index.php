fee18ce4eb5e38a119d3b9c7c831bd664c8d2c97
pradeepvarma-S <pradp786@gmail.com>
2022-04-28 19:51:35 +0100

    test

--- index.php
+++ index.php
@@ -252,7 +252,7 @@
       context.clearRect(0, 0, canvas.width, canvas.height);
 
       // draw the playfield
-      for (let row = 0; row < 20; row++) {
+      for (let row = 0; row < 18; row++) {
         for (let col = 0; col < 10; col++) {
           if (playfield[row][col]) {
             const name = playfield[row][col];
